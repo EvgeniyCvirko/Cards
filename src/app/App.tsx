fdaf9d7d7b15363e254a1f0f2845a7ffd4a106ba
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2023-01-30 23:43:25 +0300

    fix: store, index, App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import {SingIn} from '../features/SingIn/SingIn';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <SingIn/>
       </header>
     </div>
   );
---
+++ src/app/store.ts
@@ -0,0 +1,14 @@
+import {configureStore} from '@reduxjs/toolkit';
+import {combineReducers} from "redux";
+import thunkMiddleware from 'redux-thunk'
+
+export const rootReducer = combineReducers({
+
+})
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunkMiddleware)
+})
+// @ts-ignore
+window.store = store
