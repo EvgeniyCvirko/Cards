e6733f568a236b8aee976c6a3ca833bc6941f601
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2023-02-15 22:16:41 +0300

    feat: add Loading

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,23 @@
 import React, {useEffect} from 'react';
-import './App.css';
+import s from './App.module.css';
 import {Pages} from '../routing/Pages';
 import {setIsInitialized} from './AppReducer';
-import {useAppDispatch} from '../utils/hooks';
+import {useAppDispatch, useAppSelector} from '../utils/hooks';
 import {Headers} from '../components/Headers/Headers';
+import {Loading} from '../components/Loading/Loading';
+import {ErrorBar} from '../components/ErrorBar/ErrorBar';
 
 export const App = () => {
+  const isInitialized = useAppSelector(state => state.app.isInitialized)
+  const status = useAppSelector(state => state.app.status)
   const dispatch = useAppDispatch()
   useEffect(() => {
     dispatch(setIsInitialized())
   }, [])
 
+  if (!isInitialized ) {
+    return <Loading/>
+  }
   return (
     <>
       <Headers/>
