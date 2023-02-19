7002e24572f2b164efb28e0f9b36fe976fedc876
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2023-02-19 21:55:46 +0300

    feat: add EditableSpan

---
+++ src/components/common/EditableSpan/EditableSpan.tsx
@@ -0,0 +1,56 @@
+import s from './EditableSpan.module.css';
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import {Input} from 'antd';
+import {EditOutlined} from '@ant-design/icons';
+import {Button} from '../Button/Button';
+
+type EditableSpanPropsType = {
+  value: string;
+  callback: (name: string) => void
+}
+export const EditableSpan = (props: EditableSpanPropsType) => {
+  const [edit, setEdit] = useState<boolean>(false)
+  const [value, setValue] = useState<string>(props.value)
+
+  const clickHandler = () => {
+    setEdit(true)
+  }
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.currentTarget.value)
+  }
+
+  const offEditMode = () => {
+    if(value === ""){
+      setValue(props.value)
+    }
+    setEdit(false)
+    props.callback(value)
+  }
+  const onSaveHandler = () =>{
+    offEditMode()
+  }
+
+  const onPressHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+    event.code === "Enter" && offEditMode()
+  }
+  return <div className={s.editableSpan}>
+    {!edit ?
+      <>
+        <span>{props.value}</span>
+        <div className={s.editeButton} onClick={clickHandler}><EditOutlined/></div>
+      </>
+      :
+      <>
+        <Input autoFocus
+                value={value}
+                onChange={onChangeHandler}
+                onBlur={offEditMode}
+                onKeyPress={onPressHandler}
+      />
+      <Button name="save" callback={onSaveHandler}/>
+      </>
+
+    }
+
+  </div>
+}
