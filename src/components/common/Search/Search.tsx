89473a8f3602f5e89d05c1c47e2b930cb1459ae3
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2023-02-20 23:38:38 +0300

    ref: Search Component

--- src/components/common/Search/Search.tsx
+++ src/components/common/Search/Search.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
 import {Input} from 'antd';
 import {SearchOutlined} from '@ant-design/icons';
-import s from './Search.module.css'
 
-type SearchPropsType = {
-  width:string
-}
-export const Search = (props:SearchPropsType ) => {
 
-  return <div className={s.search} style={{width: props.width}}>
-    <div className={s.title}>
-      Search
-    </div>
-    <Input placeholder="Provide your text" prefix={<SearchOutlined/>} />
-  </div>
+export const Search = () => {
+
+  return <Input placeholder="Provide your text" prefix={<SearchOutlined/>}/>
 }
