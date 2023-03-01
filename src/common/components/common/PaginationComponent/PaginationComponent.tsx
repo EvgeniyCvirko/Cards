53e41345385a6601740498b879d73cd10a712246
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2023-03-01 20:16:02 +0300

    fix: change PaginationComponent

--- src/common/components/common/PaginationComponent/PaginationComponent.tsx
+++ src/common/components/common/PaginationComponent/PaginationComponent.tsx
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Pagination} from 'antd';
 import s from './PaginationComponent.module.css'
+import {startPageCount} from '../../../constants/projectConstant';
+import {useSearchParams} from 'react-router-dom';
 
 type PaginationComponentType = {
   total: number
-  changePageCount: (page: number, pageCount: number) => void
-
 }
 
 export const PaginationComponent = (props: PaginationComponentType) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [pageCount, setPageCount] = useState<number>(Number(searchParams.get('pageCount')) || startPageCount)
+  const [page, setPage] = useState<number>(Number(searchParams.get('page')))
+
   const changePageCount = (page: number, pageCount: number) => {
-    props.changePageCount(page, pageCount)
+  setPage(page)
+    setPageCount(pageCount)
+    const queryParams: { pageCount?: string, page?: string } = {}
+    queryParams.pageCount = String(pageCount)
+    queryParams.page = String(page)
+    setSearchParams({
+      ...Object.fromEntries(searchParams),
+      ...queryParams,
+    })
   }
 
   return <div className={s.pagination}><Pagination
-    defaultCurrent={1}
-    defaultPageSize={5}
+    defaultCurrent={page}
+    defaultPageSize={pageCount}
     total={props.total}
     onChange={changePageCount}
     pageSizeOptions={[5, 11, 25, 100]}
