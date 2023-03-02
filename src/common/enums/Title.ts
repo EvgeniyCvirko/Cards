8fee5e83db6857ac188893f282e4dcb96d08d204
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2023-03-02 19:31:09 +0300

    feat: add enum title

---
+++ src/common/enums/Title.ts
@@ -0,0 +1,8 @@
+export enum title {
+  addTitleCardPack = 'Add new pack',
+  editeTitleCardPack = 'Edit pack',
+  deleteTitleCardPack = 'Delete pack',
+  editeTitleCard = 'Edit card',
+  addTitleCard = 'Add new card',
+  deleteTitleCard = 'Delete card',
+}
