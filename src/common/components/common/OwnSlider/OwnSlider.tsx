d3bee0a2a3305a81e9baaf5ea789f7f698c5c327
EvgeniyCvirko <evgeniy.cvirko.90@mail.ru>
2023-03-03 15:49:32 +0300

    fix: ownSlider send Param

--- src/common/components/common/OwnSlider/OwnSlider.tsx
+++ src/common/components/common/OwnSlider/OwnSlider.tsx
@@ -1,22 +1,40 @@
 import React, {ChangeEvent, useState} from 'react';
 import s from './OwnSlider.module.css'
 import {Input, Slider} from 'antd';
+import {useSearchParams} from 'react-router-dom';
 
-type SubTitlePropsType = {}
-export const OwnSlider = (props: SubTitlePropsType) => {
+export const OwnSlider = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [minValue, setMinValue] = useState(0)
   const [maxValue, setMaxValue] = useState(110)
+  
   const onChange = (value: [number, number]) => {
     setMinValue(value[0]);
     setMaxValue(value[1]);
   };
+  const onAfterChange = (value: [number, number]) => {
+    setMinValue(value[0]);
+    setMaxValue(value[1]);
+    changeSearchSize(value[0],value[1] )
+  };
   const changeMinInput = (value: ChangeEvent<HTMLInputElement>) => {
     setMinValue(+value.currentTarget.value)
+    changeSearchSize(+value.currentTarget.value,maxValue)
   };
   const changeMaxInput = (value: ChangeEvent<HTMLInputElement>) => {
     setMaxValue(+value.currentTarget.value)
+    changeSearchSize(minValue,+value.currentTarget.value )
   };
-
+ const changeSearchSize = (minV:number, maxV: number) => {
+   const queryParams: { min?: number, max?: number} = {}
+   minV === 0 ?  searchParams.delete('min') : queryParams.min = minV
+   maxV === 110 ? searchParams.delete('max') : queryParams.max = maxV
+   // @ts-ignore
+   setSearchParams({
+     ...Object.fromEntries(searchParams),
+     ...queryParams,
+   })
+ }
 
   return <div className={s.slider}>
     <Input style={{width: '45px', marginRight: '5px'}}
@@ -26,11 +44,13 @@ export const OwnSlider = (props: SubTitlePropsType) => {
     />
     <Slider style={{flexBasis: '100%'}}
             range
+            step={1}
             value={[minValue, maxValue]}
             min={0}
             max={110}
             defaultValue={[0, 110]}
             onChange={onChange}
+            onAfterChange={onAfterChange}
     />
     <Input style={{width: '50px', marginLeft: '5px'}}
            size="small"
